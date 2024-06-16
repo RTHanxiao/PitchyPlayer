9147fe7fcca8118344f7617037a751d8f388a4ba
RTHanxiao <1034486334@qq.com>
2024-06-16 14:42:55 +0800

    你的提交信息

--- app/src/main/java/com/example/pitchdetect/MusicPlayerActivity.java
+++ app/src/main/java/com/example/pitchdetect/MusicPlayerActivity.java
@@ -9,29 +9,29 @@ import android.graphics.BitmapFactory;
 import android.media.MediaMetadataRetriever;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Handler;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.File;
 import java.io.IOException;
 
 public class MusicPlayerActivity extends AppCompatActivity {
     TextView songname, songdesc;
     ImageButton prev_btn, play_btn, next_btn;
     ImageView albumArt;
+    SeekBar seekBar;
     MediaPlayer mediaPlayer = new MediaPlayer();
     int songPos;
     int songID;
     int songCount;
     SQLiteDatabase db;
-    Cursor cursor;
+    Handler handler = new Handler();
 
     @SuppressLint("Range")
     @Override
@@ -39,17 +39,16 @@ public class MusicPlayerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music_player);
 
-        //载入数据库
         SongListDatabaseHelper dbHelper = new SongListDatabaseHelper(this);
         db = dbHelper.getReadableDatabase();
 
-        //载入歌曲信息
         songname = findViewById(R.id.songname);
         songdesc = findViewById(R.id.songdesc);
         albumArt = findViewById(R.id.imageView3);
         play_btn = findViewById(R.id.imageButton2);
         next_btn = findViewById(R.id.imageButton4);
         prev_btn = findViewById(R.id.imageButton3);
+        seekBar = findViewById(R.id.seekBar);
 
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
@@ -63,7 +62,8 @@ public class MusicPlayerActivity extends AppCompatActivity {
         String songDesc = intent.getStringExtra("songDesc");
         String songSrc = intent.getStringExtra("songSrc");
 
-        cursor = db.rawQuery("SELECT * FROM songlist", null);
+        String sql = "SELECT * FROM songlist;";
+        Cursor cursor = db.rawQuery(sql, null);
 
         Cursor mCount = db.rawQuery("SELECT count(*) FROM songlist", null);
         mCount.moveToFirst();
@@ -77,16 +77,18 @@ public class MusicPlayerActivity extends AppCompatActivity {
         setAlbumArt(songSrc);
 
         try {
-            Log.d("MusicPlayerActivity", "Trying to play: " + songSrc);
-            if (new File(songSrc).exists()) {
-                mediaPlayer.setDataSource(songSrc);
-                mediaPlayer.prepare();
-            } else {
-                Toast.makeText(this, "File does not exist: " + songSrc, Toast.LENGTH_SHORT).show();
-            }
+            mediaPlayer.setDataSource(songSrc);
+            mediaPlayer.prepare();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                playNextSong(cursor);
+            }
+        });
 
         play_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -104,79 +106,92 @@ public class MusicPlayerActivity extends AppCompatActivity {
         next_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                playNextSong();
+                playNextSong(cursor);
             }
         });
 
         prev_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                playPrevSong();
+                playPrevSong(cursor);
+            }
+        });
+
+        // 更新SeekBar的最大值和当前进度
+        seekBar.setMax(mediaPlayer.getDuration());
+        handler.post(updateSeekBar);
+
+        // 处理用户拖动SeekBar
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    mediaPlayer.seekTo(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
             }
         });
     }
 
-    private void playNextSong() {
+    private void playNextSong(Cursor cursor) {
         songID++;
-        if (songID > songCount) {
+        songPos = songID - 1;
+        if (songPos == songCount) {
+            songPos = 0;
             songID = 1;
         }
-        songPos = songID - 1;
-        playSongAtPosition(songPos);
+        playSong(cursor, songPos);
     }
 
-    private void playPrevSong() {
+    private void playPrevSong(Cursor cursor) {
         songID--;
-        if (songID < 1) {
+        songPos = songID - 1;
+        if (songPos == -1) {
+            songPos = songCount - 1;
             songID = songCount;
         }
-        songPos = songID - 1;
-        playSongAtPosition(songPos);
+        playSong(cursor, songPos);
     }
 
-    private void playSongAtPosition(int position) {
-        if (cursor.moveToPosition(position)) {
-            //读取歌曲并播放
-            String songName = cursor.getString(cursor.getColumnIndex("songname"));
-            String songSrc = cursor.getString(cursor.getColumnIndex("songsrc"));
-            String songDesc = cursor.getString(cursor.getColumnIndex("songdesc"));
-            songname.setText(songName);
-            songdesc.setText(songDesc);
-            mediaPlayer.reset();
-            try {
-                Log.d("MusicPlayerActivity", "Trying to play: " + songSrc);
-                if (new File(songSrc).exists()) {
-                    mediaPlayer.setDataSource(songSrc);
-                    mediaPlayer.prepare();
-                    mediaPlayer.start();
-                    setAlbumArt(songSrc);  // 更新专辑封面
-                    if (mediaPlayer.isPlaying()) {
-                        play_btn.setImageResource(R.drawable.ic_outline_pause_56);
-                    } else {
-                        play_btn.setImageResource(R.drawable.ic_outline_play_arrow_56);
-                    }
-                } else {
-                    Toast.makeText(this, "File does not exist: " + songSrc, Toast.LENGTH_SHORT).show();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+    private void playSong(Cursor cursor, int position) {
+        cursor.moveToPosition(position);
+        String songName = cursor.getString(cursor.getColumnIndex("songname"));
+        String songSrc = cursor.getString(cursor.getColumnIndex("songsrc"));
+        String songDesc = cursor.getString(cursor.getColumnIndex("songdesc"));
+        songname.setText(songName);
+        songdesc.setText(songDesc);
+        mediaPlayer.reset();
+        try {
+            mediaPlayer.setDataSource(songSrc);
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+            setAlbumArt(songSrc);  // 更新专辑封面
+            if (mediaPlayer.isPlaying()) {
+                play_btn.setImageResource(R.drawable.ic_outline_pause_56);
+            } else {
+                play_btn.setImageResource(R.drawable.ic_outline_play_arrow_56);
             }
+            seekBar.setMax(mediaPlayer.getDuration());  // 更新SeekBar的最大值
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
-    //设定专辑封面
+
     private void setAlbumArt(String songSrc) {
         MediaMetadataRetriever mmr = new MediaMetadataRetriever();
-        try {
-            mmr.setDataSource(songSrc);
-            byte[] artBytes = mmr.getEmbeddedPicture();
-            if (artBytes != null) {
-                Bitmap bitmap = BitmapFactory.decodeByteArray(artBytes, 0, artBytes.length);
-                albumArt.setImageBitmap(bitmap);
-            } else {
-                albumArt.setImageResource(R.drawable.background_gradient); // 设置默认封面图像
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+        mmr.setDataSource(songSrc);
+        byte[] artBytes = mmr.getEmbeddedPicture();
+        if (artBytes != null) {
+            Bitmap bitmap = BitmapFactory.decodeByteArray(artBytes, 0, artBytes.length);
+            albumArt.setImageBitmap(bitmap);
+        } else {
             albumArt.setImageResource(R.drawable.background_gradient); // 设置默认封面图像
         }
     }
@@ -185,5 +200,17 @@ public class MusicPlayerActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         mediaPlayer.reset();
+        handler.removeCallbacks(updateSeekBar);
     }
+
+    // 更新SeekBar的进度
+    private Runnable updateSeekBar = new Runnable() {
+        @Override
+        public void run() {
+            if (mediaPlayer != null) {
+                seekBar.setProgress(mediaPlayer.getCurrentPosition());
+                handler.postDelayed(this, 1000);
+            }
+        }
+    };
 }
